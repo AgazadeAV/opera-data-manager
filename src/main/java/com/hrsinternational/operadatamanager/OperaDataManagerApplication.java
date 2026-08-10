c634205cf7d63eacbac96d4c3ea0f9de71cd11ce
Azer Agazade <azer.agazade05@gmail.com>
2026-08-10 17:26:57 +0400

    initial commit

---
+++ src/main/java/com/hrsinternational/operadatamanager/OperaDataManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hrsinternational.operadatamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperaDataManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperaDataManagerApplication.class, args);
+	}
+
+}
